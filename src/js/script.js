52f5e6da9a54c5819b4e38f012f82ceda9400ff0
Nastya <nastya_-_225589@mail.ru>
2018-07-26 17:55:54 +0300

    шапка сайта: добавила hamburger

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,18 @@
 svg4everybody(); // иницализация полифила для IE
 
 $(document).ready(function(){
-  // весь ваш код c jQuery
+  $('#main-nav__toggle').on('click', function() {
+    $('#main-nav').toggleClass('main-nav--open');
+  });
+
+  $("[data-main-nav]").hide();
+  $('#main-nav__toggle').on('click', function(){
+    $('[data-main-nav]').slideToggle(250);
+    return false;
+  });
+
 });
+
 // Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
 // function ready(fn) {
 //   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
